eeb8702a3724e4b46ec115611e57fa4f8cfc3597
geode42 <geode432@gmail.com>
2023-04-26 14:52:23 -0400

    add github link plus misc changes

--- src/App.svelte
+++ src/App.svelte
@@ -9,8 +9,8 @@
 
   import { onMount } from 'svelte'
 
-  const backgroundColors = {
-    dark: '#181818',
+  const editorBackgroundColors = {
+    dark: '#1D1D1D',
     light: '#F8F8F8',
   }
 
@@ -39,6 +39,7 @@
       root: [
         [/\\\\/, 'custom-newline'],
         [/(\{|\})/, 'custom-bracket'],
+        [/(=|&=)/, 'custom-equals-sign'],
       ],
     },
   })
@@ -56,10 +57,11 @@
     inherit: true,
     rules: [
       { token: 'custom-newline', foreground: 'C000E0' },
+      { token: 'custom-equals-sign', foreground: 'C000E0' },
       { token: 'custom-bracket', foreground: '888888' },
     ],
     colors: {
-      'editor.background': backgroundColors.light,
+      'editor.background': editorBackgroundColors.light,
     },
   })
 
@@ -67,11 +69,12 @@
     base: 'vs-dark',
     inherit: true,
     rules: [
-      { token: 'custom-newline', foreground: 'A948B9' },
+      { token: 'custom-newline', foreground: 'b779c1' },
+      { token: 'custom-equals-sign', foreground: 'b779c1' },
       { token: 'custom-bracket', foreground: '888888' },
     ],
     colors: {
-      'editor.background': backgroundColors.dark,
+      'editor.background': editorBackgroundColors.dark,
     },
   })
 
@@ -99,19 +102,39 @@
     })
 
     window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (event) => {
-      monaco.editor.setTheme(event.matches ? 'tex-dark' : 'tex-light')
-      editorContainerWrapper.style.backgroundColor = event.matches ? backgroundColors.dark : backgroundColors.light
+      preferredColorScheme = event.matches ? 'dark' : 'light'
+      monaco.editor.setTheme(preferredColorScheme == 'light' ? 'tex-light' : 'tex-dark')
+      editorContainerWrapper.style.backgroundColor = event.matches ? editorBackgroundColors.dark : editorBackgroundColors.light
     })
   })
 </script>
 
-<main class="m-0 flex h-screen gap-10">
+<main class="m-0 flex h-screen">
   <div
     bind:this={editorContainerWrapper}
     class="w-full max-w-2xl py-5 pr-5"
-    style="background-color: {preferredColorScheme == 'light' ? backgroundColors.light : backgroundColors.dark}"
+    style="background-color: {preferredColorScheme == 'light' ? editorBackgroundColors.light : editorBackgroundColors.dark}"
   >
-    <div bind:this={editorContainer} class="h-full w-full" style:backgroundColor={backgroundColors.dark} />
+    <div bind:this={editorContainer} class="h-full w-full" style:backgroundColor={editorBackgroundColors.dark} />
   </div>
-  <div bind:this={preview} class="w-full overflow-scroll py-5" />
+  <div bind:this={preview} class="w-full overflow-scroll p-5" />
+
+  <a
+    href="https://github.com/geode42/geode42-matheditor"
+    aria-label="Go to source code"
+    target="_blank"
+    rel="noreferrer noopener"
+    class="absolute bottom-5 right-5 opacity-50 hover:opacity-90"
+    style:color={preferredColorScheme == 'dark' ? 'white' : 'black'}
+  >
+    <!-- GitHub's mark -->
+    <svg aria-hidden="true" width="35" viewBox="0 0 98 96" xmlns="http://www.w3.org/2000/svg"
+      ><path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"
+        fill="currentColor"
+      /></svg
+    >
+  </a>
 </main>
